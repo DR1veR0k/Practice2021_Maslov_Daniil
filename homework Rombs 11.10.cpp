a9e1008fd3d75473005294978dbe605da62e68bf
DR1veR0k <91848301+DR1veR0k@users.noreply.github.com>
2021-12-29 10:40:41 +0300

    Add files via upload

---
+++ homework Rombs 11.10.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+int main()
+{ // Program about rombs 
+    using namespace std;
+    void spacex(int space, int x);
+    int a, b;
+    cout << "Input First value " << endl;
+    cin >> a;
+    cout << "Input second value " << endl;
+    cin >> b;
+    int spaces = a - 1;
+    int stars = 1;
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            spacex(spaces, stars);
+        }
+        cout << endl;
+        if (spaces != 0)
+        {
+            spaces -= 1;
+            stars += 2;
+        }
+        else
+        {
+            spaces += 1;
+            stars -= 2;
+        }
+    }
+
+    for (int q = 0; q < a - 1; q++)
+    {
+        for (int p = 0; p < b; p++)
+        {
+            spacex(spaces, stars);
+        }
+        cout << endl;
+        spaces += 1;
+        stars -= 2;
+    }
+}
+void spacex(int space, int x)
+{
+    for (int i = 0; i < space; i++)
+    {
+        cout << " ";
+    }
+    for (int j = 0; j < x; j++)
+    {
+        cout << "*";
+    }
+    for (int i = 0; i < space; i++)
+    {
+        cout << " ";
+    }
+}
